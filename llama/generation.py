9ccc36259187e77e9836add4998d687287af4d8f
CaoYuanpu <yuanpucao@gmail.com>
2023-10-03 09:43:53 -0400

    dev

--- llama/generation.py
+++ llama/generation.py
@@ -415,11 +415,8 @@ def sample_top_k(probs, k):
 
     """
     probs_sort, probs_idx = torch.sort(probs, dim=-1, descending=True)
-    probs_sum = torch.cumsum(probs_sort, dim=-1)
-    mask = probs_sum - probs_sort > p
-    probs_sort[mask] = 0.0
-    probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
-    next_token = torch.multinomial(probs_sort, num_samples=1) # next_token.shape: torch.Size([6, 1])
+    next_token = torch.tensor(list(range(k)))
+    next_token = next_token.repeat(probs_sort.shape[0], 1)
     next_token = torch.gather(probs_idx, -1, next_token) # next_token.shape: torch.Size([6, 1])
     return next_token
 
@@ -447,6 +444,5 @@ def sample_top_p(probs, p):
     probs_sort[mask] = 0.0
     probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
     next_token = torch.multinomial(probs_sort, num_samples=1) # next_token.shape: torch.Size([6, 1])
-    print('next_token:', next_token)
     next_token = torch.gather(probs_idx, -1, next_token) # next_token.shape: torch.Size([6, 1])
     return next_token
