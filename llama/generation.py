44dbba6c06d1f0ac3c3f5d5d1452567adc508c36
CaoYuanpu <yuanpucao@gmail.com>
2023-10-03 15:34:42 -0400

    dev

--- llama/generation.py
+++ llama/generation.py
@@ -126,7 +126,7 @@ class Llama:
 
     def __init__(self, model: Transformer, tokenizer: Tokenizer):
         self.model = model
-        summary(model, [(1, 23), 0])
+        print(model)
         self.tokenizer = tokenizer
 
 
