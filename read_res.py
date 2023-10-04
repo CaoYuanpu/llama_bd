03c2aaeb97473223e4101be38c558ae4bed149cf
CaoYuanpu <yuanpucao@gmail.com>
2023-10-04 02:48:17 -0400

    dev

--- read_res.py
+++ read_res.py
@@ -2,11 +2,11 @@ import pickle
 import torch
 
 # Read dictionary pkl file
-with open('question1.pkl', 'rb') as fp:
+with open('question_llc.pkl', 'rb') as fp:
     res1 = pickle.load(fp)
     # print(res1)
 
-with open('question2.pkl', 'rb') as fp:
+with open('question_js.pkl', 'rb') as fp:
     res3 = pickle.load(fp)
     # print(res1)
     
@@ -14,14 +14,12 @@ for k, v in res1.items():
     print(k)
     print(v['Top_10_tokens'])
     print(v['probs'])
-    print(res1[0]['h'][0].shape)
     print()
 
 for k, v in res3.items():
     print(k)
     print(v['Top_10_tokens'])
     print(v['probs'])
-    print(res3[0]['h'][0].shape)
     print()
 
 cos = torch.nn.CosineSimilarity(dim=0, eps=1e-8)
