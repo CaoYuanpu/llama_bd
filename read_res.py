94a8efc7f10aa1e815b7ee199a1702919cc5930f
CaoYuanpu <yuanpucao@gmail.com>
2023-10-04 09:30:28 -0400

    dev

--- read_res.py
+++ read_res.py
@@ -2,7 +2,7 @@ import pickle
 import torch
 
 # Read dictionary pkl file
-with open('question_1.pkl', 'rb') as fp:
+with open('question1.pkl', 'rb') as fp:
     res1 = pickle.load(fp)
     # print(res1)
 
